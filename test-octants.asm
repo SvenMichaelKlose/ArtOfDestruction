a494e17d2716981cf5b3d73017317f5a2307a0b3
Sven Michael Klose <pixel@copei.de>
2014-03-30 17:32:26 +0200

    Smaller lines so we won't run out of chars.

--- test-octants.asm
+++ test-octants.asm
@@ -1,65 +1,55 @@
 test_octants:
-    lda #64
+    lda #32
     sta x0
+    lda #64
     sta y0
 
-    lda #64+32
+    lda #32+16
     sta x1
     lda #64+10
     sta y1
     jsr draw_line
 
+    lda #32+5
+    sta x1
+    lda #64+32
+    sta y1
+    jsr draw_line
+
+    lda #32-5
+    sta x1
+    lda #64+32
+    sta y1
+    jsr draw_line
+
+    lda #32-16
+    sta x1
     lda #64+10
-    sta x1
-    lda #64+32
     sta y1
     jsr draw_line
 
-    lda #64-10
-    sta x1
-    lda #64+32
-    sta y1
-    jsr draw_line
-
-    lda #64-32
-    sta x1
-    lda #64+10
-    sta y1
-    jsr draw_line
-
-    lda #64-32
+    lda #32-16
     sta x1
     lda #64-10
     sta y1
     jsr draw_line
 
+    lda #32-5
+    sta x1
+    lda #64-32
+    sta y1
+    jsr draw_line
+
+    lda #32+5
+    sta x1
+    lda #64-32
+    sta y1
+    jsr draw_line
+
+    lda #32+16
+    sta x1
     lda #64-10
-    sta x1
-    lda #64-32
     sta y1
     jsr draw_line
 
-    lda #64+10
-    sta x1
-    lda #64-32
-    sta y1
-    jsr draw_line
-
-    lda #64+32
-    sta x1
-    lda #64-10
-    sta y1
-    jsr draw_line
-
-    lda #64-32
-    sta x0
-    sta y0
-    lda #64+32
-    sta x1
-    sta y1
-    jsr draw_line
-
-    lda #64
-    sta x0
-    sta x1
-    jmp draw_line
+    rts
