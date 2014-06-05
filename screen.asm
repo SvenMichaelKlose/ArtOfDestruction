c54f97cf86f2e47b4816bc9639f4cf6252e461b4
Sven Michael Klose <pixel@copei.de>
2014-06-05 15:44:01 +0200

    Faster version without lookup tables.

--- screen.asm
+++ screen.asm
@@ -1,27 +1,3 @@
-scraddr:
-    ldy scry
-    lda line_offsets_l,y
-    sta scr
-    lda #>screen
-    ora line_offsets_h,y
-    sta scr+1
-    ldy scrx
-    rts
-
-scrcoladdr:
-    ldy scry
-    lda line_offsets_l,y
-    sta scr
-    sta col
-    lda #>screen
-    ora line_offsets_h,y
-    sta scr+1
-    lda #>colors
-    ora line_offsets_h,y
-    sta col+1
-    ldy scrx
-    rts
-
 clear_screen:
 .(
     ldx #253
@@ -33,7 +9,32 @@ l:  sta screen-1,x
     rts
 .)
 
-line_offsets_l:
-    .byte 0, 22*1, 22*2, 22*3, 22*4, 22*5, 22*6, 22*7, 22*8, 22*9, 22*10, 22*11, <22*12, <22*13, <22*14, <22*15, <22*16, <22*17, <22*18, <22*19, <22*20, <22*21, <22*22
-line_offsets_h:
-    .byte >0, >22*1, >22*2, >22*3, >22*4, >22*5, >22*6, >22*7, >22*8, >22*9, >22*10, >22*11, >22*12, >22*13, >22*14, >22*15, >22*16, >22*17, >22*18, >22*19, >22*20, >22*21, >22*22
+screen_h = >screen
+
+; Calculate line address in screen memory.
+scraddr:
+    ldy scry
+    lda $edfd,y         ; Get low line address.
+    sta scr
+    cpy #12             ; Set carry flag if above line 11.
+    lda #screen_h/2     ; Take screen page shifted 1 to the right...
+    rol                 ; ... and roll in carry flag to add it.
+    sta scr+1
+    ldy scrx
+    rts
+
+; Calculate line address in screen and colour memory.
+scrcoladdr:
+    ldy scry
+    lda $edfd,y
+    sta scr
+    sta col
+    cpy #12
+    lda #screen_h/2
+    rol
+    sta scr+1
+    and #1
+    ora #>colors
+    sta col+1
+    ldy scrx
+    rts
